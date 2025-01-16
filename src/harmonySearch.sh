d3661cffc0af7881d3d439184fd49ca15ea040ba
Meesenburg <janlaurin.meesenburg@hpc-head-n1.unitn.it>
2025-01-16 20:17:00 +0100

    rename new version

--- src/harmonySearch.sh
+++ src/harmonySearch.sh
@@ -8,16 +8,14 @@
 module load mpich-3.2
 
 #Serial
-#./Parallel-Harmony-Search/src/serial.out 1000 100000 0.9 0.3 0.01 1
-#./Parallel-Harmony-Search/src/serial.out 1000 1000000 0.9 0.3 0.01 1
-#./Parallel-Harmony-Search/src/serial.out 1000 10000000 0.9 0.3 0.01 1 # 1.5 Minutes
-#./Parallel-Harmony-Search/src/serial.out 1000 100000000 0.9 0.3 0.01 1 # 14 Minutes
-#./Parallel-Harmony-Search/src/serial.out 1000 1000000000 0.9 0.3 0.01 1 # 2 Hours 20 Minutes
+./Parallel-Harmony-Search/src/serial.out 1000 0.9 0.3 0.01 1000 5 42
+./Parallel-Harmony-Search/src/serial.out 1000 0.9 0.3 0.01 10000 5 42
+./Parallel-Harmony-Search/src/serial.out 1000 0.9 0.3 0.01 100000 5 42
+./Parallel-Harmony-Search/src/serial.out 1000 0.9 0.3 0.01 1000000 5 42
+./Parallel-Harmony-Search/src/serial.out 1000 0.9 0.3 0.01 10000000 5 42
 
 #Parallel MPI
-#./Parallel-Harmony-Search/src/serial.out 10000 1000000 0.9 0.3 0.01 42
-./Parallel-Harmony-Search/src/serial2.out 10000 0.9 0.3 0.01 1000000 5 42
-
+#./Parallel-Harmony-Search/src/serial.out 10000 0.9 0.3 0.01 1000000 5 42
 #mpirun.actual -np 1 ./Parallel-Harmony-Search/src/mpi.out 10000 1000000 0.9 0.3 0.01 1
 #mpirun.actual -np 2 ./Parallel-Harmony-Search/src/mpi.out 10000 1000000 0.9 0.3 0.01 1
 #mpirun.actual -np 4 ./Parallel-Harmony-Search/src/mpi.out 10000 1000000 0.9 0.3 0.01 1
