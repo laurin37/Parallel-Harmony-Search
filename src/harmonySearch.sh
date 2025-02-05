43a18c9768a0c71981d98d6272134e3c8a449b02
Meesenburg <janlaurin.meesenburg@hpc-head-n2.unitn.it>
2025-02-05 13:06:22 +0100

    Update execution parameters in harmonySearch.sh and add new results to harmony_search_results.csv for improved performance analysis

--- src/harmonySearch.sh
+++ src/harmonySearch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l select=1:ncpus=64:mem=2gb
 # set max execution time
-#PBS -l walltime=2:00:00
+#PBS -l walltime=6:00:00
 # imposta la coda di esecuzione
 #PBS -q short_cpuQ
 
@@ -15,14 +15,14 @@ module load mpich-3.2
 #./Parallel-Harmony-Search/src/serial.out 1000 0.9 0.3 0.01 10000000 5 42
 
 #Parallel MPI
-./Parallel-Harmony-Search/src/serial.out 1000 0.9 0.3 0.01 1000000 36 42
-mpirun.actual -np 1 ./Parallel-Harmony-Search/src/mpi.out 1000 0.9 0.3 0.01 1000000 36 42
-mpirun.actual -np 2 ./Parallel-Harmony-Search/src/mpi.out 1000 0.9 0.3 0.01 1000000 36 42
-mpirun.actual -np 4 ./Parallel-Harmony-Search/src/mpi.out 1000 0.9 0.3 0.01 1000000 36 42
-mpirun.actual -np 8 ./Parallel-Harmony-Search/src/mpi.out 1000 0.9 0.3 0.01 1000000 36 42
-mpirun.actual -np 16 ./Parallel-Harmony-Search/src/mpi.out 1000 0.9 0.3 0.01 1000000 36 42
-mpirun.actual -np 32 ./Parallel-Harmony-Search/src/mpi.out 1000 0.9 0.3 0.01 1000000 36 42
-mpirun.actual -np 64 ./Parallel-Harmony-Search/src/mpi.out 1000 0.9 0.3 0.01 1000000 36 42
+./Parallel-Harmony-Search/src/serial.out 1000 0.9 0.3 0.01 10000000 36 42
+mpirun.actual -np 1 ./Parallel-Harmony-Search/src/mpi.out 1000 0.9 0.3 0.01 10000000 36 42
+mpirun.actual -np 2 ./Parallel-Harmony-Search/src/mpi.out 1000 0.9 0.3 0.01 20000000 36 42
+mpirun.actual -np 4 ./Parallel-Harmony-Search/src/mpi.out 1000 0.9 0.3 0.01 40000000 36 42
+mpirun.actual -np 8 ./Parallel-Harmony-Search/src/mpi.out 1000 0.9 0.3 0.01 80000000 36 42
+mpirun.actual -np 16 ./Parallel-Harmony-Search/src/mpi.out 1000 0.9 0.3 0.01 160000000 36 42
+mpirun.actual -np 32 ./Parallel-Harmony-Search/src/mpi.out 1000 0.9 0.3 0.01 320000000 36 42
+mpirun.actual -np 64 ./Parallel-Harmony-Search/src/mpi.out 1000 0.9 0.3 0.01 640000000 36 42
 
 
 #Parallel OpenMP
